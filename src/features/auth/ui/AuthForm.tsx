ec5c86931f168a1ed13e28bb984794a2c980ee22
Andrey Metsler <ametsler@vp.ru>
2026-02-13 19:38:23 +0600

    День 19
    [x] Доработать типизацию хуков и адаптировать graphql-codegen под Apollo v4
    [x] Начать разработку формы входа/регистрации
    [x] Подготовить переменные в tailwind под цвета нашего дизайна

--- src/features/auth/ui/AuthForm.tsx
+++ src/features/auth/ui/AuthForm.tsx
@@ -1,30 +1,75 @@
 'use client'
 
-import { useLoginMutation } from '@/__generated__/output'
+import { LoginDocument, RegisterDocument } from '@/__generated__/graphql'
+import { useMutation } from '@apollo/client/react'
+import Link from 'next/link'
 
 import { Button } from '@/shared/components/ui/button'
 import { Input } from '@/shared/components/ui/input'
+
+import { PAGES } from '@/shared/config/page.config'
 
 interface Props {
   type: 'login' | 'register'
 }
 
 export function AuthForm({ type }: Props) {
-  const {} = useLoginMutation()
+  const isLogin = type === 'login'
+  const [register, { data, loading }] = useMutation(
+    isLogin ? LoginDocument : RegisterDocument
+  )
+
   return (
-    <div>
-      <h1>{type === 'register' ? 'Регистрация' : 'Авторизация'}</h1>
+    <div className={'flex h-screen'}>
+      <div
+        className={
+          'm-auto w-sm bg-linear-to-tr from-[#8062ee] to-[#a088fc] p-5 text-white shadow-lg'
+        }
+      >
+        <h1 className={'mb-5 text-center text-4xl font-semibold'}>
+          {isLogin ? 'Авторизация' : 'Регистрация'}
+        </h1>
 
-      <form>
-        <Input
-          type="email"
-          name={'email'}
-          placeholder={'Email'}
-          required
-        />
+        <form className={'space-y-3'}>
+          <Input
+            type="email"
+            name={'email'}
+            placeholder={'Email'}
+            required
+          />
 
-        <Button>{type === 'register' ? 'Зарегистрироваться' : 'Войти'}</Button>
-      </form>
+          <div className={'text-center'}>
+            <Button
+              type="submit"
+              disabled={loading}
+            >
+              {isLogin ? 'Войти' : 'Зарегистрироваться'}
+            </Button>
+          </div>
+        </form>
+
+        <div className={'mt-3 text-center'}>
+          {isLogin ? (
+            <div>
+              <Link
+                href={PAGES.REGISTER}
+                className={'underline'}
+              >
+                Нет аккаунта? Заполните форму регистрации
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <Link
+                href={PAGES.LOGIN}
+                className={'underline'}
+              >
+                Есть акканут?
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
