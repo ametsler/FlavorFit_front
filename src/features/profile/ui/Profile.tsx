704e045cb74df731e189498626f1c3eabf6a0c6a
Andrey Metsler <ametsler@vp.ru>
2026-05-15 18:53:31 +0600

    День 33
    [x] - Оптимизация кода, правильная подгрузка из graphql типов для формы
    [x] - Красивый скетелетон лоадер и обработка состояния загрузки
    [x] - Допиливаем визуал до идеала

--- src/features/profile/ui/Profile.tsx
+++ src/features/profile/ui/Profile.tsx
@@ -4,11 +4,43 @@ import { ProfileForm } from './ProfileForm'
 import { GetProfileDocument } from '@/__generated__/graphql'
 import { useQuery } from '@apollo/client/react'
 
+import { SkeletonLoader } from '@/shared/components/custom-ui/SkeletonLoader'
+
 export function Profile() {
   const { data, loading } = useQuery(GetProfileDocument)
 
   if (loading || !data?.me) {
-    return <div>Загрузка...</div>
+    return (
+      <div>
+        <div className="mb-6 flex items-center justify-between">
+          <SkeletonLoader
+            count={1}
+            className="w-xs"
+          />
+          <div className="flex items-center gap-2">
+            <SkeletonLoader
+              count={2}
+              className="mb-0 w-32"
+            />
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-8">
+          <div>
+            <SkeletonLoader
+              count={4}
+              className="mb-4"
+            />
+          </div>
+          <div>
+            <SkeletonLoader
+              count={6}
+              className="mb-4"
+            />
+          </div>
+        </div>
+      </div>
+    )
   }
 
   return <ProfileForm data={data} />
